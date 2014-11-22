6a6d397c7d8e5d2f8231c7c85478ace7a05cd406
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-11-22 10:06:35 -0500

    skip unimplemented tests
    This will give a clearer picture of what still needs to be implemented.

--- test/app.js
+++ test/app.js
@@ -13,12 +13,7 @@ describe('ti-fs', function() {
 		dst.write(src);
 	});
 
-	it('exports module', function() {
-		should.exist(fs);
-		fs.should.be.an.Object;
-	});
-
-	it('#Stats', function() {
+	it.skip('#Stats', function() {
 		(function() { fs.Stats(); }).should.throw(/implemented/);
 	});
 
@@ -45,11 +40,11 @@ describe('ti-fs', function() {
 		fs.existsSync('another/bad/file').should.be.false;
 	});
 
-	it('#readFile', function() {
+	it.skip('#readFile', function() {
 		(function() { fs.readFile(); }).should.throw(/implemented/);
 	});
 
-	it('#readFileSync', function() {
+	it.skip('#readFileSync', function() {
 		(function() { fs.readFileSync(); }).should.throw(/implemented/);
 	});
 
@@ -195,271 +190,271 @@ describe('ti-fs', function() {
 		});
 	});
 
-	it('#read', function() {
+	it.skip('#read', function() {
 		(function() { fs.read(); }).should.throw(/implemented/);
 	});
 
-	it('#readSync', function() {
+	it.skip('#readSync', function() {
 		(function() { fs.readSync(); }).should.throw(/implemented/);
 	});
 
-	it('#write', function() {
+	it.skip('#write', function() {
 		(function() { fs.write(); }).should.throw(/implemented/);
 	});
 
-	it('#writeSync', function() {
+	it.skip('#writeSync', function() {
 		(function() { fs.writeSync(); }).should.throw(/implemented/);
 	});
 
-	it('#rename', function() {
+	it.skip('#rename', function() {
 		(function() { fs.rename(); }).should.throw(/implemented/);
 	});
 
-	it('#renameSync', function() {
+	it.skip('#renameSync', function() {
 		(function() { fs.renameSync(); }).should.throw(/implemented/);
 	});
 
-	it('#truncate', function() {
+	it.skip('#truncate', function() {
 		(function() { fs.truncate(); }).should.throw(/implemented/);
 	});
 
-	it('#truncateSync', function() {
+	it.skip('#truncateSync', function() {
 		(function() { fs.truncateSync(); }).should.throw(/implemented/);
 	});
 
-	it('#ftruncate', function() {
+	it.skip('#ftruncate', function() {
 		(function() { fs.ftruncate(); }).should.throw(/implemented/);
 	});
 
-	it('#ftruncateSync', function() {
+	it.skip('#ftruncateSync', function() {
 		(function() { fs.ftruncateSync(); }).should.throw(/implemented/);
 	});
 
-	it('#rmdir', function() {
+	it.skip('#rmdir', function() {
 		(function() { fs.rmdir(); }).should.throw(/implemented/);
 	});
 
-	it('#rmdirSync', function() {
+	it.skip('#rmdirSync', function() {
 		(function() { fs.rmdirSync(); }).should.throw(/implemented/);
 	});
 
-	it('#fdatasync', function() {
+	it.skip('#fdatasync', function() {
 		(function() { fs.fdatasync(); }).should.throw(/implemented/);
 	});
 
-	it('#fdatasyncSync', function() {
+	it.skip('#fdatasyncSync', function() {
 		(function() { fs.fdatasyncSync(); }).should.throw(/implemented/);
 	});
 
-	it('#fsync', function() {
+	it.skip('#fsync', function() {
 		(function() { fs.fsync(); }).should.throw(/implemented/);
 	});
 
-	it('#fsyncSync', function() {
+	it.skip('#fsyncSync', function() {
 		(function() { fs.fsyncSync(); }).should.throw(/implemented/);
 	});
 
-	it('#mkdir', function() {
+	it.skip('#mkdir', function() {
 		(function() { fs.mkdir(); }).should.throw(/implemented/);
 	});
 
-	it('#mkdirSync', function() {
+	it.skip('#mkdirSync', function() {
 		(function() { fs.mkdirSync(); }).should.throw(/implemented/);
 	});
 
-	it('#readdir', function() {
+	it.skip('#readdir', function() {
 		(function() { fs.readdir(); }).should.throw(/implemented/);
 	});
 
-	it('#readdirSync', function() {
+	it.skip('#readdirSync', function() {
 		(function() { fs.readdirSync(); }).should.throw(/implemented/);
 	});
 
-	it('#fstat', function() {
+	it.skip('#fstat', function() {
 		(function() { fs.fstat(); }).should.throw(/implemented/);
 	});
 
-	it('#lstat', function() {
+	it.skip('#lstat', function() {
 		(function() { fs.lstat(); }).should.throw(/implemented/);
 	});
 
-	it('#stat', function() {
+	it.skip('#stat', function() {
 		(function() { fs.stat(); }).should.throw(/implemented/);
 	});
 
-	it('#fstatSync', function() {
+	it.skip('#fstatSync', function() {
 		(function() { fs.fstatSync(); }).should.throw(/implemented/);
 	});
 
-	it('#lstatSync', function() {
+	it.skip('#lstatSync', function() {
 		(function() { fs.lstatSync(); }).should.throw(/implemented/);
 	});
 
-	it('#statSync', function() {
+	it.skip('#statSync', function() {
 		(function() { fs.statSync(); }).should.throw(/implemented/);
 	});
 
-	it('#readlink', function() {
+	it.skip('#readlink', function() {
 		(function() { fs.readlink(); }).should.throw(/implemented/);
 	});
 
-	it('#readlinkSync', function() {
+	it.skip('#readlinkSync', function() {
 		(function() { fs.readlinkSync(); }).should.throw(/implemented/);
 	});
 
-	it('#symlink', function() {
+	it.skip('#symlink', function() {
 		(function() { fs.symlink(); }).should.throw(/implemented/);
 	});
 
-	it('#symlinkSync', function() {
+	it.skip('#symlinkSync', function() {
 		(function() { fs.symlinkSync(); }).should.throw(/implemented/);
 	});
 
-	it('#link', function() {
+	it.skip('#link', function() {
 		(function() { fs.link(); }).should.throw(/implemented/);
 	});
 
-	it('#linkSync', function() {
+	it.skip('#linkSync', function() {
 		(function() { fs.linkSync(); }).should.throw(/implemented/);
 	});
 
-	it('#unlink', function() {
+	it.skip('#unlink', function() {
 		(function() { fs.unlink(); }).should.throw(/implemented/);
 	});
 
-	it('#unlinkSync', function() {
+	it.skip('#unlinkSync', function() {
 		(function() { fs.unlinkSync(); }).should.throw(/implemented/);
 	});
 
-	it('#fchmod', function() {
+	it.skip('#fchmod', function() {
 		(function() { fs.fchmod(); }).should.throw(/implemented/);
 	});
 
-	it('#fchmodSync', function() {
+	it.skip('#fchmodSync', function() {
 		(function() { fs.fchmodSync(); }).should.throw(/implemented/);
 	});
 
-	it('#lchmod', function() {
+	it.skip('#lchmod', function() {
 		(function() { fs.lchmod(); }).should.throw(/implemented/);
 	});
 
-	it('#lchmodSync', function() {
+	it.skip('#lchmodSync', function() {
 		(function() { fs.lchmodSync(); }).should.throw(/implemented/);
 	});
 
-	it('#chmod', function() {
+	it.skip('#chmod', function() {
 		(function() { fs.chmod(); }).should.throw(/implemented/);
 	});
 
-	it('#chmodSync', function() {
+	it.skip('#chmodSync', function() {
 		(function() { fs.chmodSync(); }).should.throw(/implemented/);
 	});
 
-	it('#lchown', function() {
+	it.skip('#lchown', function() {
 		(function() { fs.lchown(); }).should.throw(/implemented/);
 	});
 
-	it('#lchownSync', function() {
+	it.skip('#lchownSync', function() {
 		(function() { fs.lchownSync(); }).should.throw(/implemented/);
 	});
 
-	it('#fchown', function() {
+	it.skip('#fchown', function() {
 		(function() { fs.fchown(); }).should.throw(/implemented/);
 	});
 
-	it('#fchownSync', function() {
+	it.skip('#fchownSync', function() {
 		(function() { fs.fchownSync(); }).should.throw(/implemented/);
 	});
 
-	it('#chown', function() {
+	it.skip('#chown', function() {
 		(function() { fs.chown(); }).should.throw(/implemented/);
 	});
 
-	it('#chownSync', function() {
+	it.skip('#chownSync', function() {
 		(function() { fs.chownSync(); }).should.throw(/implemented/);
 	});
 
-	it('#_toUnixTimestamp', function() {
+	it.skip('#_toUnixTimestamp', function() {
 		(function() { fs._toUnixTimestamp(); }).should.throw(/implemented/);
 	});
 
-	it('#utimes', function() {
+	it.skip('#utimes', function() {
 		(function() { fs.utimes(); }).should.throw(/implemented/);
 	});
 
-	it('#utimesSync', function() {
+	it.skip('#utimesSync', function() {
 		(function() { fs.utimesSync(); }).should.throw(/implemented/);
 	});
 
-	it('#futimes', function() {
+	it.skip('#futimes', function() {
 		(function() { fs.futimes(); }).should.throw(/implemented/);
 	});
 
-	it('#futimesSync', function() {
+	it.skip('#futimesSync', function() {
 		(function() { fs.futimesSync(); }).should.throw(/implemented/);
 	});
 
-	it('#writeFile', function() {
+	it.skip('#writeFile', function() {
 		(function() { fs.writeFile(); }).should.throw(/implemented/);
 	});
 
-	it('#writeFileSync', function() {
+	it.skip('#writeFileSync', function() {
 		(function() { fs.writeFileSync(); }).should.throw(/implemented/);
 	});
 
-	it('#appendFile', function() {
+	it.skip('#appendFile', function() {
 		(function() { fs.appendFile(); }).should.throw(/implemented/);
 	});
 
-	it('#appendFileSync', function() {
+	it.skip('#appendFileSync', function() {
 		(function() { fs.appendFileSync(); }).should.throw(/implemented/);
 	});
 
-	it('#watch', function() {
+	it.skip('#watch', function() {
 		(function() { fs.watch(); }).should.throw(/implemented/);
 	});
 
-	it('#watchFile', function() {
+	it.skip('#watchFile', function() {
 		(function() { fs.watchFile(); }).should.throw(/implemented/);
 	});
 
-	it('#unwatchFile', function() {
+	it.skip('#unwatchFile', function() {
 		(function() { fs.unwatchFile(); }).should.throw(/implemented/);
 	});
 
-	it('#realpathSync', function() {
+	it.skip('#realpathSync', function() {
 		(function() { fs.realpathSync(); }).should.throw(/implemented/);
 	});
 
-	it('#realpath', function() {
+	it.skip('#realpath', function() {
 		(function() { fs.realpath(); }).should.throw(/implemented/);
 	});
 
-	it('#createReadStream', function() {
+	it.skip('#createReadStream', function() {
 		(function() { fs.createReadStream(); }).should.throw(/implemented/);
 	});
 
-	it('#ReadStream', function() {
+	it.skip('#ReadStream', function() {
 		(function() { fs.ReadStream(); }).should.throw(/implemented/);
 	});
 
-	it('#FileReadStream', function() {
+	it.skip('#FileReadStream', function() {
 		(function() { fs.FileReadStream(); }).should.throw(/implemented/);
 	});
 
-	it('#createWriteStream', function() {
+	it.skip('#createWriteStream', function() {
 		(function() { fs.createWriteStream(); }).should.throw(/implemented/);
 	});
 
-	it('#WriteStream', function() {
+	it.skip('#WriteStream', function() {
 		(function() { fs.WriteStream(); }).should.throw(/implemented/);
 	});
 
-	it('#FileWriteStream', function() {
+	it.skip('#FileWriteStream', function() {
 		(function() { fs.FileWriteStream(); }).should.throw(/implemented/);
 	});
 
-	it('#SyncWriteStream', function() {
+	it.skip('#SyncWriteStream', function() {
 		(function() { fs.SyncWriteStream(); }).should.throw(/implemented/);
 	});
 
